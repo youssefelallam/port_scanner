bf7a297dac543aa3fd777b30856159cac2862ce8
Youssef El Allam <80406227+youssefelallam@users.noreply.github.com>
2021-03-15 18:39:20 +0100

    Add files via upload

---
+++ port_scanner.py
@@ -0,0 +1,15 @@
+import socket
+
+host = input("Please enter the ip address : ")
+
+try:
+    for port in range(1,2000):
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        socket.setdefaulttimeout(0.5)
+        result = s.connect_ex((host,port))
+        if result == 0:
+            name = socket.getservbyport(port)
+            print(f"Port {port} {name} is open.")
+        s.close()
+except:
+    pass
